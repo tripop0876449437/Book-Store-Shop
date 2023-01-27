63155e39870d34a5fb6fbf1a2bb5479c1fa4b761
tripop0876449437 <tripop0876449437@gmail.com>
2023-01-27 07:30:28 +0700

    Home-in-the-database-V3

--- Back-end/admin.php
+++ Back-end/admin.php
@@ -4,11 +4,51 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../CSS/navbar.css">
+  <link rel="stylesheet" href="../CSS/admin.css">
+  <link rel="stylesheet" href="../CSS/footer.css">
   <title>ผู้ดูแลระบบ</title>
 </head>
 <body>
-  <section>
-    ผู้ดูแลระบบ
+
+  <!-- Header Home Section -->
+  <?php include "../section/header.php"; ?>
+  
+  <!-- Content Home Section -->
+  <section class="home">
+    <h3>ควบคุมระบบหลังบ้าน</h3>
+    <div class="container">
+      <div class="boxs">
+        <div class="name"><h3>สินค้า</h3></div>
+        <div class="value"><p>0</p></div>
+        <div class="btn"><input type="button"value="เข้าไปควบคุม"></div>
+      </div>
+      <div class="boxs">
+        <div class="name"><h3>รายการ</h3></div>
+        <div class="value"><p>0</p></div>
+        <div class="btn"><input type="button"value="เข้าไปควบคุม"></div>        
+      </div>
+      <div class="boxs">
+        <div class="name"><h3>ตะกร้าสินค้า</h3></div>
+        <div class="value"><p>0</p></div>
+        <div class="btn"><input type="button"value="เข้าไปควบคุม"></div>
+      </div>
+      <div class="boxs">
+        <div class="name"><h3>ข้อความ</h3></div>
+        <div class="value"><p>0</p></div>
+        <div class="btn"><input type="button"value="เข้าไปควบคุม"></div>
+      </div>
+      <div class="boxs">
+        <div class="name"><h3>ผู้ใช้งาน</h3></div>
+        <div class="value"><p>0</p></div>
+        <div class="btn"><input type="button"value="เข้าไปควบคุม"></div>
+      </div>
+    </div>
   </section>
+
+  <!-- Footer Home Section -->
+  <?php include "../section/footer.php"; ?>
+
 </body>
 </html>
+
